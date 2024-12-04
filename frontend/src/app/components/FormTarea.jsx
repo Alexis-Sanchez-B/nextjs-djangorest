56a18c7f0659eb01bc0c009300ce8de7f6d89488
Alexis-Sanchez-B <sanchezalexis1994@gmail.com>
2024-12-04 17:01:27 -0300

    se modifica el back end para que permita la coneccion desde el front se hace el primer ingreso de tarea desde el front

--- frontend/src/app/components/FormTarea.jsx
+++ frontend/src/app/components/FormTarea.jsx
@@ -1,23 +1,54 @@
-import React from 'react'
+"use client"
+import { useState } from 'react'
 
 function FormTarea() {
-  return (
-    <div className='bg-slate-200 p-7'>
-        <form action=''>
-            <h1 className='text-black font-bold '>Añadir Tarea</h1>
 
-            <label htmlFor="titulo" className='text-sm text-black'>Titulo:</label>
+    const [titulo, setTitulo] = useState('')
+    const [descripcion, setDescripcion] = useState('')
 
-            <input className='bg-slate-400 rounded-md p-2 mb-2 block w-full text-black' type="text" name='titulo' placeholder='ingresa tituto' />
+    const handleSubmit = async (e) => {
+        e.preventDefault()
 
-            <label htmlFor="Descripcion" className='text-sm text-black'>Descripcion:</label>
+        // Esperar la respuesta de fetch
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/tareas/`, {
+            method: 'POST',
+            body: JSON.stringify({ titulo, descripcion }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
 
-            <textarea className='bg-slate-400 rounded-md p-2 mb-2 block w-full text-black' name="descripcion" placeholder='ingresa descripcion'  ></textarea>
+        // Verificar si la respuesta fue exitosa
+        if (!res.ok) {
+            // Si no es exitosa, lanzar un error
+            console.error('Error en la solicitud:', res.statusText)
+            return
+        }
 
-            <button className='bg-indigo-500 text-white rounded-md p-2 block w-full' >Save</button>
-        </form>
-    </div>
-  )
+        // Obtener los datos en formato JSON
+        const data = await res.json()
+
+        // Ver los datos en la consola
+        console.log(data)
+    }
+
+    return (
+        <div className='bg-slate-200 p-7'>
+            <form onSubmit={handleSubmit}>
+                <h1 className='text-black font-bold'>Añadir Tarea</h1>
+
+                <label htmlFor="titulo" className='text-sm text-black'>Titulo:</label>
+                <input className='bg-slate-400 rounded-md p-2 mb-2 block w-full text-black' type="text" name='titulo' placeholder='ingresa titulo'
+                    onChange={e => setTitulo(e.target.value)} />
+
+                <label htmlFor="descripcion" className='text-sm text-black'>Descripcion:</label>
+                <textarea className='bg-slate-400 rounded-md p-2 mb-2 block w-full text-black' name="descripcion" placeholder='ingresa descripcion'
+                    onChange={e => setDescripcion(e.target.value)}></textarea>
+
+                <button className='bg-indigo-500 text-white rounded-md p-2 block w-full'>Save</button>
+            </form>
+        </div>
+    )
 }
 
 export default FormTarea
